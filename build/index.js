c5a560eb33c307a669041bb911d98caac6270b26
ArjunVenkitraman <ramukpully@gmail.com>
2023-02-27 16:38:59 +0530

    task1

---
+++ build/index.js
@@ -0,0 +1,22 @@
+"use strict";
+class Employee {
+    constructor(emp_id, name, designation, salary, project) {
+        this.emp_id = emp_id;
+        this.name = name;
+        this.designation = designation;
+        this.salary = salary;
+        this.project = project;
+    }
+    display() {
+        let dis = `Employee id ${this.emp_id}\nEmployee Name : ${this.name}\nEmployee Designation : ${this.designation}\nEmployee salary : ${this.salary}\nEmployee Project : ${this.project}\n`;
+        // console.log(`Employee id : ${this.emp_id}`);
+        // console.log(`Employee Name : ${this.name}`);
+        // console.log(`Employee Designation : ${this.designation}`)
+        // console.log(`Employee salary : ${this.salary}`)
+        // console.log(`Employee Project : ${this.project}`)
+        console.log(dis);
+    }
+}
+let obj = new Employee(101, "Venkit", "Developer", 20000, "HTML");
+//console.log(obj.emp_id);
+obj.display();
